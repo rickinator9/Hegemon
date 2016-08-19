01893b98287b3be4ba1f838f83ed3303be13f226
rickinator9 <rickvisser94@hotmail.com>
2016-08-19 19:28:49 +0200

    Added Zygote.

---
+++ Assets/Source/Contexts/GameContext/Model/Characters/IZygote.cs
@@ -0,0 +1,9 @@
+﻿namespace Assets.Source.Contexts.GameContext.Model.Characters
+{
+    public interface IZygote
+    {
+        Gender Gender { get; set; }
+        
+        int Fetuses { get; set; } 
+    }
+}
